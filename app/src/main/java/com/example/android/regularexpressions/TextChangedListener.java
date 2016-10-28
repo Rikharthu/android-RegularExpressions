8a47e2ce6a5547327de87f0cf357daa937594aef
ricard.arman.kuodis <ricard.arman.kuodis@accenture.com>
2016-10-28 17:59:40 +0300

    fix

---
+++ app/src/main/java/com/example/android/regularexpressions/TextChangedListener.java
@@ -0,0 +1,31 @@
+package com.example.android.regularexpressions;
+
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public abstract class TextChangedListener<T> implements TextWatcher {
+    private T target;
+
+    public TextChangedListener(T target) {
+        this.target = target;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        // not used
+    }
+
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        // not used
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        this.onTextChanged(target, s);
+    }
+
+    public abstract void onTextChanged(T target, Editable s);
+}
